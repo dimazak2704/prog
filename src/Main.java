d4bae61483048cd4e743ff1eb79d897b7960b087
dimazak2704 <103667513+dimazak2704@users.noreply.github.com>
2022-09-30 22:21:37 +0300

    added 3 sites, sorted first site with hashmap

--- src/Main.java
+++ src/Main.java
@@ -3,43 +3,74 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.IOException;
+import java.util.*;
 
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Document doc = Jsoup.connect("https://terramare.vn.ua/menu-dostavki/pizza/").get();
+
+        HashMap<String, String > map = new HashMap<>();
+        LinkedHashMap<String, String> sortedMap = new LinkedHashMap<>();
+        ArrayList<String> list = new ArrayList<>();
 
 
-        Elements pizzas = doc.getElementsByClass("product-item");
-
-
+        System.out.println("ПЕРШИЙ САЙТ");
+        Document doc = Jsoup.connect("https://la.ua/vinnytsya/").get();
+        Elements pizzas = doc.getElementsByClass("col-md-6 col-lg-4");
         for (Element pizza : pizzas){
-
-            String title = pizza.getElementsByClass("woocommerce-loop-product__title").text();
-
-            String price = pizza.getElementsByClass("woocommerce-Price-amount amount").text();
-            String symb = pizza.getElementsByClass("woocommerce-Price-currencySymbol").text();
-            String desc = pizza.getElementsByAttributeValue("itemprop", "description").text();
-
-
-            System.out.print(title + " " + price + "   |   " + desc);
-            System.out.println(" ");
-
+          //String title = pizza.getElementsByClass("productTitle h5").text();
+          String desc = pizza.getElementsByClass("productDescription ").text();
+          String price = pizza.getElementsByClass("productPrice").text();
+          System.out.println(price + "\t" + desc);
 
         }
-        
-        /*Document doc1 = Jsoup.connect("https://express-pizza.vn.ua/catalog/pitstsa/").get();
+
+
+        System.out.println("\n" + "\n" + "ДРУГИЙ САЙТ" + "\n" + "\n");
+        Document doc1 = Jsoup.connect("https://express-pizza.vn.ua/catalog/pitstsa/").get();
         Elements pizzas1 = doc1.getElementsByClass("col-lg-3 col-xl-3 col-sm-6 product-ajax-cont ");
-
         for (Element pizza1 : pizzas1){
-            String desc = pizza1.getElementsByAttributeValue("itemprop", "name").text();
-            String pizzaaaa = pizza1.getElementsByAttributeValue("itemprop", "price").text();
-            String opus = pizza1.getElementsByAttributeValue("itemprop", "description").text();
+            String title1 = pizza1.getElementsByAttributeValue("itemprop", "name").text();
+            String price1 = pizza1.getElementsByAttributeValue("itemprop", "price").text();
+            String desc1 = pizza1.getElementsByAttributeValue("itemprop", "description").text();
+            map.put(title1, price1);
+            //System.out.println(title1 + "   " + "\t" + price1 + " грн" + "\t" + desc1);
+        }
 
-            System.out.println(desc + "   " + "\t" + pizzaaaa + " грн" + "\t" + opus);
-        }*/
 
+        System.out.println("\n" + "\n" + "ТРЕТІЙ САЙТ" + "\n" + "\n");
+        Document doc2 = Jsoup.connect("https://paparoni.com.ua/").get();
+        Elements pizzas2 = doc2.getElementsByClass("isotope-item align-left tm-has-options product type-product post-192 status-publish first instock product_cat-20 has-post-thumbnail shipping-taxable purchasable product-type-simple");
+        for (Element pizza2 : pizzas2){
+            String title2 = doc2.getElementsByClass("desc").text();
+            System.out.println(title2);
+        }
+        //int a =
+
+
+
+
+        //sorting
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            list.add(entry.getValue());
+        }
+        Collections.sort(list, new Comparator<String>() {
+            public int compare(String str, String str1) {
+                return (str).compareTo(str1);
+            }
+        });
+        for (String str : list) {
+            for (Map.Entry<String, String> entry : map.entrySet()) {
+                if (entry.getValue().equals(str)) {
+                    sortedMap.put(entry.getKey(), str);
+                }
+            }
+        }
+
+        for (String s : sortedMap.keySet()){
+            System.out.println(s + " = " + map.get(s));
+        }
     }
 }
